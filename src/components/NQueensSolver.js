880b6517eeb72db1fb3e9a63e2a636f33e13fcc7
Danford Nchembi <danfordnchembi@gmail.com>
2023-10-20 18:50:09 +0100

    Adding a spinner and alert for in-runtime issues

--- src/components/NQueensSolver.js
+++ src/components/NQueensSolver.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Chessboard from "./Chessboard";
 import solveNQueens from "../algorithms/nQueensAlgorithm";
-import { Button, Typography, Row, Col, Input } from "antd";
+import { Button, Typography, Row, Col, Input, Spin, Alert } from "antd";
 
 function NQueensSolver() {
   const [n, setN] = useState(4);
@@ -9,18 +9,32 @@ function NQueensSolver() {
   const [currentSolution, setCurrentSolution] = useState(0);
   const queensPositions = solutions[currentSolution];
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [isWarning, setIsWarning] = useState(false);
+
   const { Title } = Typography;
 
   const handleInputChange = (e) => {
-    setN(parseInt(e.target.value));
-    setSolutions([]);
-    setCurrentSolution(0);
+    let numberOfQueensEntered = e.target.value;
+
+    if (numberOfQueensEntered <= 12) {
+      setN(parseInt(numberOfQueensEntered));
+      setSolutions([]);
+      setCurrentSolution(0);
+    } else {
+      setTimeout(() => {
+        setIsWarning(true);
+      }, 1000);
+    }
+
   };
 
   const handleSolve = (e) => {
     e.preventDefault();
-    setSolutions(solveNQueens(n));
-    setCurrentSolution(0);
+    if (n <= 12) {
+      setSolutions(solveNQueens(n));
+      setCurrentSolution(0);
+    }
   };
 
   const handleSwitchSolution = (direction) => (e) => {
@@ -39,17 +53,31 @@ function NQueensSolver() {
       {/* Input Form */}
       <form>
         <Title level={2}>Enter the number of queens (n):</Title>
-        <Row style={{paddingBottom:"10px"}}>
+        <Row style={{ paddingBottom: "10px" }}>
           <Col md={2}>
             <Input type="number" value={n} onChange={handleInputChange} />
           </Col>
-          <Col md={4} style={{paddingLeft:"5px"}}>
+          <Col md={2} style={{ paddingLeft: "5px" }}>
             <Button type="primary" shape="round" onClick={handleSolve}>
               Solve
             </Button>
           </Col>
+          <Col>{isLoading ? <Spin /> : ""}</Col>
         </Row>
       </form>
+
+      <Row style={{ paddingBottom: "10px" }}>
+        {" "}
+        {/* Display Alert prompting the user that exceeding 12 can be expensive to computer} */}
+        {isWarning ? (
+          <Alert
+            message="Exceeding the value of 12 can take very long to compute. Please grab a cup of tea as we work."
+            type="error"
+          />
+        ) : (
+          ""
+        )}
+      </Row>
 
       {/* Chessboard */}
       {/* Display the chessboard here */}
@@ -67,8 +95,17 @@ function NQueensSolver() {
             <Title level={5}>
               Current solution in text: {solutions[currentSolution]}
             </Title>
-            <Button type="primary" onClick={handleSwitchSolution("prev")}>Previous</Button>
-            <Button type="primary" danger style={{marginLeft:"4px"}} onClick={handleSwitchSolution("next")}>Next</Button>
+            <Button type="primary" onClick={handleSwitchSolution("prev")}>
+              Previous
+            </Button>
+            <Button
+              type="primary"
+              danger
+              style={{ marginLeft: "4px" }}
+              onClick={handleSwitchSolution("next")}
+            >
+              Next
+            </Button>
           </div>
         </Col>
       </Row>
